790ec030f156dac038a6ce0ff4d180a79613ca09
Lunatc <cintia.braz@unesp.br>
2024-05-11 14:19:29 -0300

    Learning variable types

---
+++ Basics/Types.java
@@ -0,0 +1,16 @@
+public class Types {
+    public static void main(String[] args){
+        
+        String name = "Jonh";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        boolean myBool = true;
+        System.out.println(myBool);
+
+        float myFloatNum = 5.99f;
+        System.out.println(myFloatNum);
+    }
+}
