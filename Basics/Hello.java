68264a7699368cadbf8e935950aecda0f0b623ed
Lunatc <cintia.braz@unesp.br>
2024-05-11 14:13:04 -0300

    First Java program

---
+++ Basics/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        
+        System.out.print("I will print on the same line");
+        
+        System.out.println(2+2);
+        
+        //This is a comment
+        
+        /*this is also a 
+        comment */
+
+    }
+}
