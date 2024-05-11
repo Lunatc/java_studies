6b29555a90666cba9a2a155ee0572a0888bd587c
Lunatc <cintia.braz@unesp.br>
2024-05-11 14:24:29 -0300

    Declaring variables

--- Basics/Types.java
+++ Basics/Types.java
@@ -12,5 +12,22 @@ public class Types {
 
         float myFloatNum = 5.99f;
         System.out.println(myFloatNum);
+
+        System.out.println("Hello " + name);
+        String lastName = "Doe";
+
+        String fullName = name + lastName;
+        System.out.println(fullName);
+
+        //Declare Many Variables
+
+        int x = 5, y = 6, z = 60;
+        System.out.println(x + y + z);
+
+        //One value to multiple variables
+
+        x = y = z = 50;
+        System.out.println(x + y + z);
+
     }
 }
