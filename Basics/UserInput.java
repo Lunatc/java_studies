965426f7ff85a0e7e31ba070f4231fbb514c7e6f
Lunatc <cintia.braz@unesp.br>
2024-06-12 12:55:22 -0300

    Different types of inputs

--- Basics/UserInput.java
+++ Basics/UserInput.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 class UserInput {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter username");
+        System.out.println("Enter username, age, salary");
 
+        //String input
         String userName = myObj.nextLine();
+
+        //Numerical input
+        int age = myObj.nextInt();
+        double salary = myObj.nextDouble();
+
         System.out.println("Username is: " + userName);
+        System.out.println("Age: " + age);
+        System.out.println("Salary " + salary);
     }
 }
